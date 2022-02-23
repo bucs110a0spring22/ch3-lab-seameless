259dc2bd79213617392c4278a92657918f94ffda
Seamus Madden <98534241+seameless@users.noreply.github.com>
2022-02-23 00:42:34 +0000

    --

--- README.md
+++ README.md
@@ -5,21 +5,20 @@
 
 ***
 
-_Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Two races were performed in Part 1, the first being a race of one value between 1 and 100, and the second being a series of 10 independent values between 1 and 10 for each turtle. Part B consists of making 5 shapes with a for loop with one of the turtles.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 1
 
-Grace days remaining: < # >/5
+Grace days remaining: 4/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ None
 
 ## REFERENCES:
- < List any outside resources used >
+ None
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ None
--- main.py
+++ main.py
@@ -16,11 +16,63 @@ michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
 leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
+michelangelo.speed(1)
+leonardo.speed(1)
 
 ## 5. your code goes here
 
+##Race 1
+x = random.randrange(1,100)
+y = random.randrange(1,100)
+leonardo.forward(x)
+michelangelo.forward(y)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
+##Race 2
+for i in range(10) :
+  a = random.randrange(1,10)
+  b = random.randrange(1,10)
+  leonardo.forward(a)
+  michelangelo.forward(b)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+  
 # Part B - complete part B here
 
+for i in range(3) :
+  a = 25
+  b = (360 / 3)
+  michelangelo.down()
+  michelangelo.forward(a)
+  michelangelo.right(b)
+michelangelo.clear()
 
+for i in range(4) :
+  a = 25
+  b = (360 / 4)
+  michelangelo.forward(a)
+  michelangelo.right(b)
+michelangelo.clear()
+
+for i in range(6) :
+  a = 25
+  b = (360 / 6)
+  michelangelo.forward(a)
+  michelangelo.right(b)
+michelangelo.clear()
+
+for i in range(9) :
+  a = 25
+  b = (360 / 9)
+  michelangelo.forward(a)
+  michelangelo.right(b)
+michelangelo.clear()
+
+for i in range(12) :
+  a = 25
+  b = (360 / 12)
+  michelangelo.forward(a)
+  michelangelo.right(b)
+michelangelo.clear()
 window.exitonclick()
